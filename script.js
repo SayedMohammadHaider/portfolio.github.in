263ca510b8a4c31f4b010a8c067f07168a448548
Sayed Mohammad Haider <Sayed.Haider@in.ey.com>
2022-08-23 13:31:56 +0530

    modified skill section

--- script.js
+++ script.js
@@ -36,14 +36,67 @@ $(document).ready(function () {
     });
 });
 
+var skills = [
+    {
+        "Name": "C#",
+        "Progress": "80%"
+    },
+    {
+        "Name": "LINQ",
+        "Progress": "70%"
+    },
+    {
+        "Name": "Dot Net Core",
+        "Progress": "70%"
+    },
+    {
+        "Name": "Core WCF",
+        "Progress": "70%"
+    },
+    {
+        "Name": "Entity Framework",
+        "Progress": "65%"
+    },
+    {
+        "Name": "Azure",
+        "Progress": "50%"
+    },
+    {
+        "Name": "Sharepoint",
+        "Progress": "50%"
+    },
+    {
+        "Name": "SQL",
+        "Progress": "50%"
+    },
+    {
+        "Name": "React JS",
+        "Progress": "50%"
+    },
+    {
+        "Name": "HTML/CSS",
+        "Progress": "40%"
+    }
+]
+
+buildSkillDesign();
+function buildSkillDesign() {
+    var design = '';
+    for (var i = 0; i < skills.length; i++) {
+        design += '<div class="skillBox"> <div class="skillText"> <p>' + skills[i].Name + '</p> </div> <div class="skillContaier" style="width:' + skills[i].Progress + '"> <div class="skill">' + skills[i].Progress +'</div> </div> </div>';
+    }
+    var skillBox = document.getElementById("boxSkill");
+    skillBox.innerHTML = design;
+}
+
 calculateAge();
 function calculateAge() {
-    var x = document.getElementById("aa");
+    var ageText = document.getElementById("age");
     const now = new Date();
     var date = new Date();
     date.setFullYear(1994, 3, 19);
     const diff = Math.abs(now - date);
     const age = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
-    x.innerHTML = '<span> age : </span>' + age;
+    ageText.innerHTML = '<span> age : </span>' + age;
 }
 
